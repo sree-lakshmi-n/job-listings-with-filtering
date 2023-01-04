fe0a96cccc76366c2a93a6877b3fab3ea0294e2c
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-04 14:54:40 +0530

    adding tags

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,16 +1,31 @@
 // exporting function to replace template
-const path = require("path");
 
-module.exports = (temp, item) => {
+module.exports = (temp, tempTags, item) => {
   let output = temp.replace(/{%COMPANY_NAME%}/g, item.company);
-  output = output.replace(
-    /{%COMPANY_LOGO%}/g,
-    `${path.join(__dirname, "../", item.logo)}`
-  );
+  output = output.replace(/{%COMPANY_LOGO%}/g, `${item.logo}`);
   output = output.replace(/{%POSITION%}/g, item.position);
   output = output.replace(/{%POSTED_AT%}/g, item.postedAt);
   output = output.replace(/{%CONTRACT%}/g, item.contract);
   output = output.replace(/{%LOCATION%}/g, item.location);
   output = output.replace(/{%SKILLS%}/g, [...item.languages, ...item.tools]);
+
+  // adding tags
+  if (item.new && item.featured) {
+    output = output.replace(
+      /{%TAGS%}/g,
+      tempTags.replace(/{%TAG%}/g, "new") +
+        tempTags.replace(/{%TAG%}/g, "featured")
+    );
+  } else if (item.new) {
+    output = output.replace(/{%TAGS%}/g, tempTags.replace(/{%TAG%}/g, "new"));
+  } else if (item.featured) {
+    output = output.replace(
+      /{%TAGS%}/g,
+      tempTags.replace(/{%TAG%}/g, "featured")
+    );
+  } else {
+    output = output.replace(/{%TAGS%}/g, "");
+  }
+
   return output;
 };
