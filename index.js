d85715f4de11b60396f263d00710f7a312444779
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-04 14:55:27 +0530

    enabling static files inside public using express to display images in it.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const replaceTemplate = require("./modules/replaceTemplate");
+const path = require("path");
 
-app.use(express.static("./assets/images"));
-app.use("/images", express.static("./assets/images"));
+app.use(express.static("public"));
+
 // Read templates
 const tempOverview = fs.readFileSync(
   `${__dirname}/views/template-overview.html`,
@@ -14,19 +15,26 @@ const tempCard = fs.readFileSync(
   `${__dirname}/views/template-item-card.html`,
   "utf-8"
 );
+const tempTags = fs.readFileSync(
+  `${__dirname}/views/template-tags.html`,
+  "utf-8"
+);
 
 // Read data
-const data = JSON.parse(fs.readFileSync(`${__dirname}/assets/data.json`));
+const data = JSON.parse(
+  fs.readFileSync(`${__dirname}/public/assets/data.json`)
+);
 console.log(data[0].logo);
 app.get("/", (req, res) => {
   res.writeHead(200, {
     "Content-type": "text/html",
   });
   const itemCards = data.map((item) => {
-    return replaceTemplate(tempCard, item);
+    return replaceTemplate(tempCard, tempTags, item);
   });
+
   const output = tempOverview.replace(/{%ITEM_CARDS%}/g, itemCards);
-  res.end(output);
+  res.end(output.split(",").join(" "));
 });
 
 const port = 3000;
