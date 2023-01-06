df0a5e483f0e258dad9514c4d836e55d1e7cd4d0
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-06 12:18:14 +0530

    Search bar added

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const replaceTemplate = require("./modules/replaceTemplate");
-const path = require("path");
 
 app.use(express.static("public"));
 
@@ -23,22 +22,37 @@ const tempSkills = fs.readFileSync(
   `${__dirname}/views/template-skill.html`,
   "utf-8"
 );
+const tempSearchBar = fs.readFileSync(
+  `${__dirname}/views/template-search-bar.html`,
+  "utf-8"
+);
+const tempSearchFilter = fs.readFileSync(
+  `${__dirname}/views/template-search-filter.html`,
+  "utf-8"
+);
 
 // Read data
 const data = JSON.parse(
   fs.readFileSync(`${__dirname}/public/assets/data.json`)
 );
-app.get("/", (req, res) => {
+
+// Search bar added
+tempOverview.replace(/{%SEARCH_BAR%}/g, tempSearchBar);
+
+// Initial Page Setup
+const pageSetup = (req, res) => {
   res.writeHead(200, {
     "Content-type": "text/html",
   });
   const itemCards = data.map((item) => {
     return replaceTemplate(tempCard, tempTags, tempSkills, item);
   });
-
   const output = tempOverview.replace(/{%ITEM_CARDS%}/g, itemCards);
+
   res.end(output.split(",").join(" "));
-});
+};
+
+app.route("/").get(pageSetup);
 
 const port = 3000;
 app.listen(port, () => console.log("listening to port", port));
