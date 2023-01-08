c86d74f0268fd2c5bce6fa1c01da609bc052bb9d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-08 06:21:07 +0530

    Displaying corresponding skill item cards on click

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,11 @@
 const skillFilter = new Set([]);
 $(document).ready(function () {
+  // Displaying corresponding skill item cards on click
   $(".skill").click(function () {
     skillFilter.add($(this).text());
+    $(".item-card").hide();
+    skillFilter.forEach((skill) => {
+      $(".item-card").find(`.${skill}`).parent().parent().parent().show();
+    });
   });
 });
