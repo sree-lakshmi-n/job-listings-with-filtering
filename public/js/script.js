8641bed00a54e30024c933c5019f70386bb8fd77
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-09 17:36:12 +0530

    moved filterSkills functionality to separate functions

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,21 @@ const skillFilter = new Set([]);
 $(document).ready(function () {
   // Displaying corresponding skill item cards on click
   $(".skill").click(function () {
-    skillFilter.add($(this).text());
-    $(".item-card").hide();
-    skillFilter.forEach((skill) => {
-      $(".item-card").find(`.${skill}`).parent().parent().parent().show();
-    });
+    const skillItem = $(this).text();
+    filterSkills(skillItem);
   });
 });
+
+const hideItemCards = () => {
+  $(".item-card").hide();
+};
+const showItemCards = (classes) => {
+  $(`.item-card${classes || ""}`).show();
+};
+
+const filterSkills = (skillItem) => {
+  skillFilter.add(skillItem);
+  hideItemCards();
+  const classes = [...skillFilter].map((skill) => `.${skill}`).join("");
+  showItemCards(classes);
+};
