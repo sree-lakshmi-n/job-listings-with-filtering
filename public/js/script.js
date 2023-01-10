03576a0b9d45e6c6584659f9efe2887433b0576a
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-10 06:23:52 +0530

    createSkillItem functionality

--- public/js/script.js
+++ public/js/script.js
@@ -20,3 +20,22 @@ const filterSkills = (skillItem) => {
   const classes = [...skillFilter].map((skill) => `.${skill}`).join("");
   showItemCards(classes);
 };
+
+const createSkillItem = (skill) => {
+  console.log("hi");
+  const skillItem = document.createElement("div");
+  skillItem.setAttribute("class", "skill-items");
+  const skillName = document.createElement("span");
+  skillName.setAttribute("class", "skill-name");
+  skillName.textContent = skill;
+  const btnClose = document.createElement("button");
+  btnClose.setAttribute("class", "btn-close");
+  btnClose.textContent = "X";
+  skillItem.appendChild(skillName);
+  skillItem.appendChild(btnClose);
+
+  const searchBar = document.getElementsByClassName("search-bar")[0];
+  console.log(searchBar);
+  searchBar.appendChild(skillItem);
+};
+createSkillItem("CSS");
